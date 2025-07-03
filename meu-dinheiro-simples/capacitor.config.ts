573b07c351f6ab470aaa8b37411b1b19409bc015
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-07-03 13:43:40 -0300

    Estrutura Ionic

---
+++ meu-dinheiro-simples/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'meu.dinheiro.simples',
+  appName: 'meu-dinheiro-simples',
+  webDir: 'www'
+};
+
+export default config;
---
+++ meu-dinheiro-simples/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBhl7knY1B2-mmuExmX0IjjBP-gVt0rRVM",
+    authDomain: "meu-dinheiro-simples.firebaseapp.com",
+    projectId: "meu-dinheiro-simples",
+    storageBucket: "meu-dinheiro-simples.firebasestorage.app",
+    messagingSenderId: "537424553084",
+    appId: "1:537424553084:web:4c14ec4eb4586a27bd55f9"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
