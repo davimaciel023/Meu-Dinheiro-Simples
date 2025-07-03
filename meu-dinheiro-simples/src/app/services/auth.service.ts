f05134aef0bbd0d950ae2cf519dadd860cd3d00f
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-07-03 14:15:19 -0300

    configurando services

--- meu-dinheiro-simples/src/app/services/auth.service.ts
+++ meu-dinheiro-simples/src/app/services/auth.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Auth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
+import {  User } from 'firebase/auth';
+import { BehaviorSubject } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
+  currentUser$ = new BehaviorSubject<User | null>(null);
 
-  constructor() { }
+  constructor(private auth: Auth) {
+
+    this.auth.onAuthStateChanged(user => this.currentUser$.next(user));
+  }
+
+  login(email: string, password: string) {
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  register(email: string, password: string) {
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(this.auth, provider);
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
+
+  getUserId(): string | null {
+    return this.auth.currentUser?.uid || null;
+  }
 }
--- meu-dinheiro-simples/src/app/services/transacao.service.ts
+++ meu-dinheiro-simples/src/app/services/transacao.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, addDoc, collectionData, query, where } from '@angular/fire/firestore';
+import { AuthService } from './auth.service';
+import { Transacao } from '../models/transacao.model';
+import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class TransacaoService {
+  constructor(private firestore: Firestore, private authService: AuthService) {}
 
-  constructor() { }
+  addTransacao(transacao: Transacao) {
+    const userId = this.authService.getUserId();
+    if (!userId) throw new Error("Usuário não autenticado");
+
+    const ref = collection(this.firestore, `users/${userId}/transactions`);
+    return addDoc(ref, transacao);
+  }
+
+  getTransacoes(): Observable<Transacao[]> {
+    const userId = this.authService.getUserId();
+    if (!userId) throw new Error("Usuário não autenticado");
+
+    const ref = collection(this.firestore, `users/${userId}/transactions`);
+    return collectionData(ref, { idField: 'id' }) as Observable<Transacao[]>;
+  }
 }
