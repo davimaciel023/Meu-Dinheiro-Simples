9e394a1406eb195e58485bdf5266711be7266892
davimaciel077@gmail.com <davimaciel077@gmail.com>
2025-07-06 20:58:10 -0300

    configurando pagina de transações

--- meu-dinheiro-simples/src/app/pages/transacoes/transacoes.page.ts
+++ meu-dinheiro-simples/src/app/pages/transacoes/transacoes.page.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { TransacaoService } from 'src/app/services/transacao.service';
+import { Transacao } from 'src/app/models/transacao.model';
+import { AsyncPipe, NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-transacoes',
-  templateUrl: './transacoes.page.html',
-  styleUrls: ['./transacoes.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, AsyncPipe, NgIf, NgFor],
+  templateUrl: './transacoes.page.html',
+  styleUrls: ['./transacoes.page.scss']
 })
 export class TransacoesPage implements OnInit {
+  transacoes: Transacao[] = []
+  carregando = true
 
-  constructor() { }
+  constructor(
+    private transacaoService: TransacaoService
+  ) { }
 
   ngOnInit() {
+    this.transacaoService.getTransacoes().subscribe((dados) => {
+      this.transacoes = dados.sort((a, b) => +new Date(b.date) - +new Date(a.date))
+
+      this.carregando = false
+    })
+  }
+
+  formatarData(data: string) {
+    return new Date(data).toLocaleDateString('pt-BR');
   }
 
 }
